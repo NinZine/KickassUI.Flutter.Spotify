7882e0febbf2f6890bd5df3d9245fb7d3b5b2775
Andreas Kröhnke <andreas@krohnke.me>
2018-01-31 21:21:40 +0900

    Most of home screen completed.

---
+++ lib/player_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+const Color _kDefaultBackgroundColor = const Color(0xff424242);
+
+class PlayerBar extends StatelessWidget implements PreferredSizeWidget {
+
+  /// Creates a tab bar in the iOS style.
+  PlayerBar({
+    Key key,
+    this.child,
+    this.backgroundColor: _kDefaultBackgroundColor
+  }) : super(key: key);
+
+  final Widget child;
+
+  /// The background color of the tab bar. If it contains transparency, the
+  /// tab bar will automatically produce a blurring effect to the content
+  /// behind it.
+  final Color backgroundColor;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(42.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final double bottomPadding = MediaQuery.of(context).padding.bottom;
+    Widget result = new DecoratedBox(
+      decoration: new BoxDecoration(
+        border: const Border(
+          top: const BorderSide(
+            color: Colors.white,
+            width: 0.0, // One physical pixel.
+            style: BorderStyle.solid,
+          ),
+          bottom: const BorderSide(
+            color: Colors.black,
+            width: 1.0,
+            style: BorderStyle.solid
+          )
+        ),
+        color: backgroundColor,
+      ),
+      // TODO(xster): allow icons-only versions of the tab bar too.
+      child: new SizedBox(
+        height: 50 + bottomPadding,
+        child: new Container(
+          child: child
+        ),
+      ),
+    );
+
+    return result;
+  }
+}
